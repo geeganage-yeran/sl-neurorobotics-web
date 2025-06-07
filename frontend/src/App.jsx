762c6c685a44d970c8327700f696e9b9b3a37967
piyumi weerasinghe <piyumiweerasinghe201@gmail.com>
2025-06-07 10:42:41 +0530

    header/footer component added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Footer from './components/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-lg text-amber-500'>
+        SL Neurorobotics
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default App
