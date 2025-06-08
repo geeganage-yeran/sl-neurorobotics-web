35bcc6b3b9f16679872cf4a55833a5816f783aef
yeran lakvidu <geeganageyeran@gmail.com>
2025-06-08 23:41:13 +0530

    About page updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import AboutPage from "./pages/AboutPage";
+import AddCart from "./pages/addtocart";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +13,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/addcart" element={<AddCart />} />
+
       </Routes>
     </BrowserRouter>
   );
