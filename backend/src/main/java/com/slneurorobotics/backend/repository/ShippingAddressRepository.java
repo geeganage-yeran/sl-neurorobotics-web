b272e669733be97e7190fbaf02a35bd2174aaa91
Nisal Ranasinghe <sachindunisal09@gmail.com>
2025-07-01 22:37:35 +0530

    cart page default address fetch completed

--- backend/src/main/java/com/slneurorobotics/backend/repository/ShippingAddressRepository.java
+++ backend/src/main/java/com/slneurorobotics/backend/repository/ShippingAddressRepository.java
@@ -12,6 +12,18 @@ public interface ShippingAddressRepository extends JpaRepository<Shipping_addres
 
     @Query("SELECT s FROM Shipping_address s WHERE s.createdBy = ?1")
     List<Shipping_address> findByAddress(long userid);
+<<<<<<< Updated upstream
+=======
+
+    @Query("SELECT sa FROM Shipping_address sa WHERE sa.createdBy = :userId AND sa.isDefault = true")
+    List<Shipping_address> findByCreatedByAndIsDefaultTrue(@Param("userId") Long userId);
+
+    @Query("SELECT sa FROM Shipping_address sa WHERE sa.createdBy = :userId AND sa.isDefault = true AND sa.id != :currentId")
+    List<Shipping_address> findByCreatedByAndIsDefaultTrueAndIdNot(@Param("userId") Long userId, @Param("currentId") Long currentId);
+
+    @Query("SELECT s FROM Shipping_address s WHERE s.createdBy = ?1 AND s.isDefault = true")
+    Shipping_address findByDefaultAddress(long userid);
+>>>>>>> Stashed changes
 }
 
 
